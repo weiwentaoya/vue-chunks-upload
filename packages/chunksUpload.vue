f65e610aa08270a6bbcc0d1f9a517e8c40cdc72e
weiwentao <17600626753@163.com>
2021-09-22 15:24:22 +0800

    fix

--- packages/chunksUpload.vue
+++ packages/chunksUpload.vue
@@ -275,7 +275,6 @@
                                 window.requestIdleCallback(workLoop)
                             }
                         })
-                        
                     }
                     window.requestIdleCallback(workLoop)
 
